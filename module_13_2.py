eea51cafe3b8753c32229f5614c32dc299944bea
Danil Terentev <danil.terentyev@gmail.com>
2024-10-09 18:04:34 +0300

    Update module_13_2.py

--- module_13_2.py
+++ module_13_2.py
@@ -7,12 +7,12 @@ bot = Bot(token = api)
 dp = Dispatcher(bot, storage= MemoryStorage())
 
 @dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    await print('Привет! Я бот помогающий твоему здоровью.')
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
 
 @dp.message_handler()
-async def all_messages(message: types.Message):
-    await print('Введите команду /start, чтобы начать общение.')
+async def all_messages(message):
+    print('Введите команду /start, чтобы начать общение.')
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
